16d155267fc3cd0c7f52a6822ccb3436827fcbf4
Erasmo Soares <erasmosaraujo@gmail.com>
2019-12-15 17:45:34 -0500

    navbar

---
+++ CoinFac.Presentation/ClientApp/src/app/common/globals.ts
@@ -0,0 +1,3 @@
+export const GlobalVariable = Object.freeze({
+    COINFAC_THEME_MODE_: 'dark-mode-none',
+});
--- CoinFac.Presentation/ClientApp/src/app/components/navbar/navbar.component.ts
+++ CoinFac.Presentation/ClientApp/src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { GlobalVariable } from 'src/app/common/globals';
 
 @Component({
-  selector: 'app-navbar',
+  selector: 'navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
 
+  private theme = GlobalVariable.COINFAC_THEME_MODE_;
+
   constructor() { }
 
   ngOnInit() {
+    console.log(this.theme);
   }
 
 }
