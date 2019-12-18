22d57433c8a0f1d82cda7ad758f9cb8ab331a9e9
Erasmo Soares <erasmosaraujo@gmail.com>
2019-12-17 22:32:18 -0500

    main-data

---
+++ CoinFac.Presentation/ClientApp/src/app/pages/main/main-data.ts
@@ -0,0 +1,246 @@
+export var accounts = [
+  {
+    id: 1,
+    name: 'Renda Fixa',
+    type: 'Income',
+    comments: 'Conta criada usando o aplicativo easynvest',
+    goal: 9000,
+    series: [
+      {
+        id: 1,
+        name: '04/12/2017',
+        value: 2650
+      },
+      {
+        id: 2,
+        name: '02/01/2018',
+        value: 4800      
+      },
+      {
+        id: 3,
+        name: '04/01/2018',
+        value: 9000
+      }
+    ]
+  },
+  {
+    id: 2,
+    name: 'Poupança',
+    type: 'Income',
+    comments: 'Poupança Bradesco',
+    goal: 5000,
+    series: [
+      {
+        id: 1,
+        name: '04/12/2017',
+        value: 2500
+      },
+      {
+        id: 2,
+        name: '02/01/2018',
+        value: 3100
+      },
+      {
+        id: 3,
+        name: '04/01/2018',
+        value: 350
+      }
+    ]
+  },
+  {
+    id: 3,
+    name: 'Corrente Bradesco',
+    type: 'Income/Expense',
+    comments: 'Conta Salário',
+    goal: 7000,
+    series: [
+      {
+        id: 1,
+        name: '04/12/2017',
+        value: 2500
+      },
+      {
+        id: 2,
+        name: '02/01/2018',
+        value: 3100
+      },
+      {
+        id: 3,
+        name: '04/01/2018',
+        value: 350
+      }
+    ]
+  },
+  {
+    id: 4,
+    name: 'Corrente Brasil',
+    type: 'Income/Expense',
+    comments: 'Conta antiga',
+    goal: 8000,
+    series: [
+      {
+        id: 1,
+        name: '04/12/2017',
+        value: 2500
+      },
+      {
+        id: 2,
+        name: '02/01/2018',
+        value: 3100
+      },
+      {
+        id: 3,
+        name: '04/01/2018',
+        value: 350
+      }
+    ]
+  },
+  {
+    id: 5,
+    name: 'Nu Conta',
+    type: 'Income/Expense',
+    comments: 'Conta com rendimento 100% CDI',
+    goal: 2000,
+    series: [
+      {
+        id: 1,
+        name: '04/12/2017',
+        value: 2500
+      },
+      {
+        id: 2,
+        name: '02/01/2018',
+        value: 3100
+      },
+      {
+        id: 3,
+        name: '04/01/2018',
+        value: 350
+      }
+    ]
+  },
+  {
+    id: 6,
+    name: 'Bitcoin (Real)',
+    type: 'Income/Expense',
+    comments: 'Bitcoins estão em baixa',
+    goal: 300,
+    series: [
+      {
+        id: 1,
+        name: '04/12/2017',
+        value: 2500
+      },
+      {
+        id: 2,
+        name: '02/01/2018',
+        value: 3100
+      },
+      {
+        id: 3,
+        name: '04/01/2018',
+        value: 350
+      }
+    ]
+  },
+  {
+    id: 7,
+    name: 'Neon',
+    type: 'Income/Expense',
+    comments: 'Conta digital usada pelo Samsung Pay',
+    goal: 300,
+    series: [
+      {
+        id: 1,
+        name: '04/12/2017',
+        value: 2500
+      },
+      {
+        id: 2,
+        name: '02/01/2018',
+        value: 3100
+      },
+      {
+        id: 3,
+        name: '04/01/2018',
+        value: 350
+      }
+    ]
+  }
+];
+
+export var single = [
+    {
+      "name": "Renda Fixa",
+      "value": 8940000
+    },
+    {
+      "name": "Pouupança",
+      "value": 5000000
+    },
+    {
+      "name": "Corrente Bradesco",
+      "value": 7200000
+    },
+    {
+      "name": "Corrente Brasil",
+      "value": 5210000
+    },
+    {
+      "name": "Nu Conta",
+      "value": 9315000
+    },
+    {
+      "name": "Bitcoin (Real)",
+      "value": 300000
+    },
+    {
+      "name": "Neon ",
+      "value": 800000
+    }
+  ];
+  
+  export var multi = [
+    {
+      "name": "Germany",
+      "series": [
+        {
+          "name": "2010",
+          "value": 7300000
+        },
+        {
+          "name": "2011",
+          "value": 8940000
+        }
+      ]
+    },
+  
+    {
+      "name": "USA",
+      "series": [
+        {
+          "name": "2010",
+          "value": 7870000
+        },
+        {
+          "name": "2011",
+          "value": 8270000
+        }
+      ]
+    },
+  
+    {
+      "name": "France",
+      "series": [
+        {
+          "name": "2010",
+          "value": 5000002
+        },
+        {
+          "name": "2011",
+          "value": 5800000
+        }
+      ]
+    }
+  ];
+  
