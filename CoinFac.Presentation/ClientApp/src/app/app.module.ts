ffa58bd469e5fdb5f5c92e5a90684ddc0539654a
Erasmo Soares <erasmosaraujo@gmail.com>
2019-12-17 22:55:42 -0500

    order-module

--- CoinFac.Presentation/ClientApp/src/app/app.module.ts
+++ CoinFac.Presentation/ClientApp/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
+import { OrderModule } from 'ngx-order-pipe';
+
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
@@ -49,6 +51,7 @@ import { AuthGuard } from './components/auth.guard';
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
+    OrderModule,
     RouterModule.forRoot([
       //{ path: '', component: HomeComponent, pathMatch: 'full' },
       //{ path: 'counter', component: CounterComponent },
