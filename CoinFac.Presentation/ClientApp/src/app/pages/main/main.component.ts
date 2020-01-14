a63ef9877bb9a459bd3d711547aae84aaab43b95
Erasmo <erasmosaraujo@gmail.com>
2020-01-13 23:05:09 -0500

    Configure user to Db in Main Component

--- CoinFac.Presentation/ClientApp/src/app/pages/main/main.component.ts
+++ CoinFac.Presentation/ClientApp/src/app/pages/main/main.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { multi } from './main-data';
 import { accounts, single } from './../main/main-data';
+import { AuthService } from '../../services/auth.service';
 //import { accounts, single, } from './../main/fac-data';
 
 @Component({
@@ -9,7 +10,8 @@ import { accounts, single } from './../main/main-data';
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
-  
+
+  profileStorage: any;
   single: any[];
   multi: any[];
   accounts: any[];
@@ -19,14 +21,24 @@ export class MainComponent implements OnInit {
     domain: ['#808782', '#a6d3a0', '#b3ffb3', '#d1ffd7']
   };
 
-  constructor() {
-    Object.assign(this, {single,multi,accounts}) 
+  constructor(public auth: AuthService) {
+
+    this.configureUser();
+
+    Object.assign(this, { single, multi, accounts })
    }
 
   ngOnInit() {
+    
+    alert("main" + JSON.stringify(this.profileStorage));
   }
 
   onSelect(event) {
     console.log(event);
+  }
+
+  configureUser(){
+    this.auth.userProfile$.subscribe(user => this.profileStorage = user);
+    this.profileStorage = JSON.parse(sessionStorage.getItem('profile'))
   }
 }
--- CoinFac.Presentation/ClientApp/src/app/services/auth.service.ts
+++ CoinFac.Presentation/ClientApp/src/app/services/auth.service.ts
@@ -10,7 +10,7 @@ import { Http } from '@angular/http';
   providedIn: 'root'
 })
 export class AuthService {
-  // Create an observable of Auth0 instance of client
+  
   auth0Client$ = (from(
     createAuth0Client({
       domain: "coinfac.auth0.com",
@@ -18,13 +18,10 @@ export class AuthService {
       redirect_uri: `${window.location.origin}`
     })
   ) as Observable<Auth0Client>).pipe(
-    shareReplay(1), // Every subscription receives the same shared value
+    shareReplay(1), 
     catchError(err => throwError(err))
   );
-  // Define observables for SDK methods that return promises by default
-  // For each Auth0 SDK method, first ensure the client instance is ready
-  // concatMap: Using the client instance, call SDK method; SDK returns a promise
-  // from: Convert that resulting promise into an observable
+ 
   isAuthenticated$ = this.auth0Client$.pipe(
     concatMap((client: Auth0Client) => from(client.isAuthenticated())),
     tap(res => this.loggedIn = res)
@@ -32,22 +29,18 @@ export class AuthService {
   handleRedirectCallback$ = this.auth0Client$.pipe(
     concatMap((client: Auth0Client) => from(client.handleRedirectCallback()))
   );
-  // Create subject and public observable of user profile data
+
   private userProfileSubject$ = new BehaviorSubject<any>(null);
   userProfile$ = this.userProfileSubject$.asObservable();
-  // Create a local property for login status
+
   loggedIn: boolean = null;
 
   constructor(private router: Router, private http: Http) {
-    // On initial load, check authentication state with authorization server
-    // Set up local auth streams if user is already authenticated
+   
     this.localAuthSetup();
-    // Handle redirect from Auth0 login
     this.handleAuthCallback();
   }
 
-  // When calling, options can be passed if desired
-  // https://auth0.github.io/auth0-spa-js/classes/auth0client.html#getuser
   getUser$(options?): Observable<any> {
     return this.auth0Client$.pipe(
       concatMap((client: Auth0Client) => from(client.getUser(options))),
@@ -57,17 +50,14 @@ export class AuthService {
   }
 
   private localAuthSetup() {
-    // This should only be called on app initialization
-    // Set up local authentication streams
+    
     const checkAuth$ = this.isAuthenticated$.pipe(
       concatMap((loggedIn: boolean) => {
         if (loggedIn) {
-          // If authenticated, get user and set in app
-          // NOTE: you could pass options here if needed
-         
+     
           return this.getUser$();
         }
-        // If not authenticated, return stream that emits 'false'
+
         return of(loggedIn);
       })
     );
@@ -75,11 +65,8 @@ export class AuthService {
   }
 
   login(redirectPath: string = '/') {
-    // A desired redirect path can be passed to login method
-    // (e.g., from a route guard)
-    // Ensure Auth0 client instance exists
+   
     this.auth0Client$.subscribe((client: Auth0Client) => {
-      // Call method to log in
       client.loginWithRedirect({
         redirect_uri: `${window.location.origin}`,
         appState: { target: redirectPath }
@@ -87,37 +74,24 @@ export class AuthService {
     });
   }
 
-  //private addUser(user: any): Observable<any> {
-    
-  //  return this.http.post("https://localhost:44372/api/user", JSON.stringify(user))
-  //    .pipe(catchError(err => throwError(err)));
-  //}
-
   private handleAuthCallback() {
-    // Call when app reloads after user logs in with Auth0
+
     const params = window.location.search;
     if (params.includes('code=') && params.includes('state=')) {
-      let targetRoute: string; // Path to redirect to after login processsed
+      let targetRoute: string; 
       const authComplete$ = this.handleRedirectCallback$.pipe(
-        // Have client, now call method to handle auth callback redirect
         tap(cbRes => {
-          // Get and set target redirect route from callback results
           targetRoute = cbRes.appState && cbRes.appState.target ? cbRes.appState.target : '/';
         }),
         concatMap(() => {
-          // Redirect callback complete; get user and login status
           return combineLatest([
             this.getUser$(),
-            this.isAuthenticated$,
-            alert("Post/User")
-            //this.addUser(this.getUser$())
+            this.isAuthenticated$
           ]);
         })
       );
-      // Subscribe to authentication completion observable
-      // Response will be an array of user and login status
+     
       authComplete$.subscribe(([user, loggedIn]) => {
-        // Redirect to target route after callback processing
         this.router.navigate([targetRoute]);
       });
     }
@@ -126,10 +100,7 @@ export class AuthService {
 
   logout() {
 
-
-    // Ensure Auth0 client instance exists
     this.auth0Client$.subscribe((client: Auth0Client) => {
-      // Call method to log out
       client.logout({
         client_id: "DfFPaitLxLB425jH0iv4EoX50Iqx91fl",
         returnTo: `${window.location.origin}`
