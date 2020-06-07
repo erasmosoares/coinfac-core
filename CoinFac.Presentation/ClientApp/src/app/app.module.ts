888a358713e1e1fca086e09da72cef641b99bc48
Erasmo <erasmosaraujo@gmail.com>
2020-06-06 23:24:41 -0400

    Update angular 9

--- CoinFac.Presentation/ClientApp/src/app/app.module.ts
+++ CoinFac.Presentation/ClientApp/src/app/app.module.ts
@@ -1,37 +1,37 @@
-import { AccountComponentService } from './pages/account/account.component.service';
-import { OrderModule } from 'ngx-order-pipe';
-import { AngularFontAwesomeModule } from 'angular-font-awesome';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
+import { AccountComponentService } from "./pages/account/account.component.service";
+import { OrderModule } from "ngx-order-pipe";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgModule } from "@angular/core";
 
-import { HttpModule } from '@angular/http';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpModule } from "@angular/http";
+import { HttpClientModule } from "@angular/common/http";
 
-import { AppComponent } from './app.component';
-import { MainComponent } from './pages/main/main.component';
-import { NgxChartsModule } from '@swimlane/ngx-charts';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { NotFoundComponent } from './pages/not-found/not-found.component';
-import { AccountComponent } from './pages/account/account.component';
-import { IncomeComponent } from './pages/income/income.component';
-import { ExpenseComponent } from './pages/expense/expense.component';
-import { ForeignCurrencyComponent } from './pages/foreign-currency/foreign-currency.component';
-import { PredictionComponent } from './pages/prediction/prediction.component';
-import { BlockchainComponent } from './pages/blockchain/blockchain.component';
-import { RouterModule } from '@angular/router';
-import { JumbotronComponent } from './components/jumbotron/jumbotron.component';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatTabsModule } from '@angular/material/tabs';
-import { NgxSmartModalModule, NgxSmartModalService } from 'ngx-smart-modal';
-import { AccountModalComponent } from './pages/account-modal/account-modal.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { TitleCasePipe } from './pipes/titlecase.pipe';
-import { AuthGuard } from './components/auth.guard';
-import { ProfileComponent } from './components/profile/profile.component';
-import { ToastrModule } from 'ngx-toastr';
-import { DatePipe } from '@angular/common';
-import { AccountModalService } from './pages/account-modal/account.modal.service';
+import { AppComponent } from "./app.component";
+import { MainComponent } from "./pages/main/main.component";
+import { NgxChartsModule } from "@swimlane/ngx-charts";
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { NotFoundComponent } from "./pages/not-found/not-found.component";
+import { AccountComponent } from "./pages/account/account.component";
+import { IncomeComponent } from "./pages/income/income.component";
+import { ExpenseComponent } from "./pages/expense/expense.component";
+import { ForeignCurrencyComponent } from "./pages/foreign-currency/foreign-currency.component";
+import { PredictionComponent } from "./pages/prediction/prediction.component";
+import { BlockchainComponent } from "./pages/blockchain/blockchain.component";
+import { RouterModule } from "@angular/router";
+import { JumbotronComponent } from "./components/jumbotron/jumbotron.component";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatTabsModule } from "@angular/material/tabs";
+import { NgxSmartModalModule, NgxSmartModalService } from "ngx-smart-modal";
+import { AccountModalComponent } from "./pages/account-modal/account-modal.component";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { TitleCasePipe } from "./pipes/titlecase.pipe";
+import { AuthGuard } from "./components/auth.guard";
+import { ProfileComponent } from "./components/profile/profile.component";
+import { ToastrModule } from "ngx-toastr";
+import { DatePipe } from "@angular/common";
+import { AccountModalService } from "./pages/account-modal/account.modal.service";
 
 @NgModule({
   declarations: [
@@ -48,7 +48,7 @@ import { AccountModalService } from './pages/account-modal/account.modal.service
     JumbotronComponent,
     AccountModalComponent,
     ProfileComponent,
-    TitleCasePipe
+    TitleCasePipe,
   ],
   imports: [
     BrowserModule,
@@ -57,28 +57,56 @@ import { AccountModalService } from './pages/account-modal/account.modal.service
     BrowserAnimationsModule,
     NgxChartsModule,
     HttpModule,
-    HttpClientModule, 
-    AngularFontAwesomeModule,
+    HttpClientModule,
+    FontAwesomeModule,
     OrderModule,
     MatToolbarModule,
     MatTabsModule,
-    ToastrModule.forRoot(), 
+    ToastrModule.forRoot(),
     NgxSmartModalModule.forChild(),
     NgxSmartModalModule.forRoot(),
     RouterModule.forRoot([
-      { path: 'home', component: MainComponent },
-      { path: 'accounts', component: AccountComponent, canActivate: [AuthGuard] },
-      { path: 'income', component: IncomeComponent, canActivate: [AuthGuard] },
-      { path: 'expense', component: ExpenseComponent, canActivate: [AuthGuard] },
-      { path: 'foreigncurrency', component: ForeignCurrencyComponent, canActivate: [AuthGuard] },
-      { path: 'prediction', component: PredictionComponent, canActivate: [AuthGuard] },
-      { path: 'blockchain', component: BlockchainComponent, canActivate: [AuthGuard] },
-      { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
-      { path: '**', component: MainComponent },
-    ])
+      { path: "home", component: MainComponent },
+      {
+        path: "accounts",
+        component: AccountComponent,
+        canActivate: [AuthGuard],
+      },
+      { path: "income", component: IncomeComponent, canActivate: [AuthGuard] },
+      {
+        path: "expense",
+        component: ExpenseComponent,
+        canActivate: [AuthGuard],
+      },
+      {
+        path: "foreigncurrency",
+        component: ForeignCurrencyComponent,
+        canActivate: [AuthGuard],
+      },
+      {
+        path: "prediction",
+        component: PredictionComponent,
+        canActivate: [AuthGuard],
+      },
+      {
+        path: "blockchain",
+        component: BlockchainComponent,
+        canActivate: [AuthGuard],
+      },
+      {
+        path: "profile",
+        component: ProfileComponent,
+        canActivate: [AuthGuard],
+      },
+      { path: "**", component: MainComponent },
+    ]),
   ],
-  providers: [NgxSmartModalService,AccountComponentService,AccountModalService],
+  providers: [
+    NgxSmartModalService,
+    AccountComponentService,
+    AccountModalService,
+  ],
   exports: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- CoinFac.Presentation/ClientApp/src/app/pages/account-modal/account-modal.component.ts
+++ CoinFac.Presentation/ClientApp/src/app/pages/account-modal/account-modal.component.ts
@@ -7,7 +7,7 @@ import {
   AfterViewInit,
 } from "@angular/core";
 import { NgxSmartModalService } from "ngx-smart-modal";
-import { FormBuilder, Validators, FormGroup } from "@angular/forms";
+import { FormBuilder, Validators, FormArray, FormGroup } from "@angular/forms";
 import { UsernameValidators } from "./username.validators";
 import { AccountService } from "src/app/services/account.service";
 import { ToastrService } from "ngx-toastr";
@@ -22,10 +22,13 @@ import { completeAccountsForTest } from "./../main/main-data";
 })
 export class AccountModalComponent implements OnInit {
   accountForm: FormGroup;
+  dynamicForm: FormGroup;
+
   capitalAccount = new CapitalAccount();
   dataForEdition: CapitalAccount;
   goalInput = "0.00";
   accounts: any[];
+  submitted = false;
 
   @Output() refreshAccounts: EventEmitter<CapitalAccount> = new EventEmitter<
     CapitalAccount
@@ -41,6 +44,11 @@ export class AccountModalComponent implements OnInit {
   ) {}
 
   ngOnInit() {
+    this.dynamicForm = this.fb.group({
+      numberOfTickets: ["", Validators.required],
+      tickets: new FormArray([]),
+    });
+
     this.accountForm = this.fb.group({
       name: [
         "",
@@ -61,6 +69,59 @@ export class AccountModalComponent implements OnInit {
     });
 
     this.accounts = completeAccountsForTest;
+  }
+
+  // convenience getters for easy access to form fields
+  get f() {
+    return this.dynamicForm.controls;
+  }
+  get t() {
+    return this.f.tickets as FormArray;
+  }
+
+  onChangeTickets(e) {
+    const numberOfTickets = e.target.value || 0;
+    if (this.t.length < numberOfTickets) {
+      for (let i = this.t.length; i < numberOfTickets; i++) {
+        this.t.push(
+          this.fb.group({
+            name: ["", Validators.required],
+            email: ["", [Validators.required, Validators.email]],
+          })
+        );
+      }
+    } else {
+      for (let i = this.t.length; i >= numberOfTickets; i--) {
+        this.t.removeAt(i);
+      }
+    }
+  }
+
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.dynamicForm.invalid) {
+      return;
+    }
+
+    // display form values on success
+    alert(
+      "SUCCESS!! :-)\n\n" + JSON.stringify(this.dynamicForm.value, null, 4)
+    );
+  }
+
+  onReset() {
+    // reset whole form back to initial state
+    this.submitted = false;
+    this.dynamicForm.reset();
+    //this.t.clear();
+  }
+
+  onClear() {
+    // clear errors and reset ticket fields
+    this.submitted = false;
+    this.t.reset();
   }
 
   loadEditForm(account: CapitalAccount) {
